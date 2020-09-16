61850a1c51aa9011ca5907da480fbf7737412031
suvendu9674410530@gmail.com <suvendu9674410530@gmail.com>
2020-09-16 10:18:05 +0530

    add centos 7 and 8 dockerfile, modify src/index.php

--- src/index.php
+++ src/index.php
@@ -1,3 +1,9 @@
-<?php
-echo "Hello AEM!! Thanks for your excellent support";
-?>
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <p>Hello AEM!! Thanks for your excellent support</p>
+ </body>
+</html>
+
